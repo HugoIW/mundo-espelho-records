e4b560342687927e2b012cbe06e85f73db8aa11d
Hugo Silva <hugo.infaweb@gmail.com>
2023-06-27 20:13:04 -0300

    added database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.ATLAS_MONGODB_CONNECTION || 'mongodb://localhost/nest',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
